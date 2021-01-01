ec52b3c51de1e798fb3456615a2da2eb3a10563a
like <980650920@qq.com>
2021-01-01 20:28:54 +0800

    cookie update

--- src/urllib/06-cookie的登录.py
+++ src/urllib/06-cookie的登录.py
@@ -17,11 +17,22 @@ from http import cookiejar
 def cookieTest():
     # 1.代码登录
     # 1.1 登录的网站
-    loginUrl = "https://mnote.tingkl.com/user/signIn"
+    loginUrl = "https://clogin.lianjia.com/authentication/authenticate"
     # 1.2登录的参数
     loginFormData = {
-        "email": "980650920@qq.com",
-        "password": "980650920"
+        "credential": {
+            "encodeVersion": "2",
+            "username": "13789983260",
+            "password": "Lk980650920"
+        },
+        "loginTicketId": "yclz6njR3xSNMvEHM1jNEWC6zcot3SKa",
+        "mainAuthMethodName": "username-password",
+        "service": "https://ajax.api.lianjia.com/login/login/getuserinfo",
+        "srcId": "eyJ0Ijoie1wiZGF0YVwiOlwiNzQ2MTFmZmIyZWYzMTViOTBhYzkxZWUyMDc2NzA3ZDFkMGQ4NzE4YWM5M2VlNDJjOTA4YWVmOTk0YjJlYzdhYTQxNWEzYzViMmI1MTFhZDc1ZWViNGI5NTU5OThlYjcyMzExNzVjYTg4M2E2Y2YyY2YwYTFlMGJiNGQ2MDkwZjljZTFmYzg1ZDFlNGU4ZTE0NWVmNzgyYWE2ZTliYzk4OTZlM2U1NTQzNjJlYmY2ZmIxNGQxMTk0YTY3MTYxMTg2NjRjYjVjOWU4MGE5M2FmZjU5OWUxZjdlMTMyMzdkMjk1MDA0ZmE2YjRmNmRiNDhlMDQ4MjNhNDgzZmMxOGYyNmRlOGUyYjM5YWI4ZTUyNDc0ZWNlYzYxMzFiMGNlZjRmZDc3NzhhNDA4MTVjMzhmNGE2MjU1ODhjNGNkYzdkN2RmYzY1ZWM2NWYxNmQ0ZjM2NmQ1MTRjYmI3MjllYjI3NlwiLFwia2V5X2lkXCI6XCIxXCIsXCJzaWduXCI6XCIxN2I5ZjBhN1wifSIsInIiOiJodHRwczovL2JqLmxpYW5qaWEuY29tLz91dG1fc291cmNlPWJhaWR1JnV0bV9tZWRpdW09cGluemh1YW4mdXRtX3Rlcm09Ymlhb3RpJnV0bV9jb250ZW50PWJpYW90aW1pYW9zaHUmdXRtX2NhbXBhaWduPXd5YmVpamluZyIsIm9zIjoid2ViIiwidiI6IjAuMSJ9",
+        "ticketMaxAge": "604800",
+        "version": "2.0",
+        "accountSystem": "customer",
+        "context": "{}"
     }
     header = {
         # 浏览器的版本
@@ -39,17 +50,18 @@ def cookieTest():
     opener = request.build_opener(cookieHandler)
 
     # 发送post请求
-    login_request = request.Request(loginUrl, headers=header, data=logStr)
+    login_request = request.Request(loginUrl, headers=header, data=logStr, method="post")
     # 如果登录成功，cookjar 自动保存cookie
     opener.open(login_request)
 
     # 2.访问个人中心
-    centerUrl = "https://mnote.tingkl.com/main.html?_from=https%3A%2F%2Fmnote.tingkl.com%2Findex.html"
+    centerUrl = "https://user.lianjia.com/"
     centerRequest = request.Request(centerUrl, headers=header)
     resp = opener.open(centerRequest)
     data = resp.read().decode('utf-8')
     with open("test.html", "w") as f:
         f.write(data)
     resp.decode('utf-8')
+
 
 cookieTest()
