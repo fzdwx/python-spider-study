1da608271817d55f3d80a5962e5a8f17148e77b0
like <980650920@qq.com>
2021-01-01 20:45:09 +0800

    cookie update

---
+++ src/urllib/07.http-errot.py
@@ -0,0 +1,10 @@
+# urllib.requst 提示错误 httpError UrlError
+
+import urllib.request
+
+url = "http://www.baidu.com"
+response = urllib.request.urlopen(url)
+
+date = response.read().decode('utf-8')
+with open("baidu.html", "w", encoding='utf-8') as f:
+    f.write(date)
