db07eb240d5e67dc763a7e8e656b8a3b3297878c
like <980650920@qq.com>
2021-02-14 12:52:08 +0800

    update

--- src/day-07/02.selenium.py
+++ src/day-07/02.selenium.py
@@ -11,7 +11,8 @@ import time
 chrome = webdriver.Chrome()
 # 输入百度url 跳转页面
 chrome.get('http://www.baidu.com')
-
-time.sleep(3)  # 3s后关闭
+chrome.find_element_by_id('kw').send_keys('药水哥')
+chrome.find_element_by_id('su').click()
+print(chrome.page_source)
 # 关闭
-chrome.quit()
+# chrome.quit()
