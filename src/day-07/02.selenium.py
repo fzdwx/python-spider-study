8591d2e0d2290f8acef2f862bde8ed0338fc5000
like <980650920@qq.com>
2021-02-14 12:46:02 +0800

    selenium 测试

---
+++ src/day-07/02.selenium.py
@@ -0,0 +1,17 @@
+# - - - - - - - - - - - 
+# @author like
+# @since 2021-02-14 12:32
+# @email 980650920@qq.com
+#
+from selenium import webdriver
+from selenium import common
+import time
+
+# 打开浏览器
+chrome = webdriver.Chrome()
+# 输入百度url 跳转页面
+chrome.get('http://www.baidu.com')
+
+time.sleep(3)  # 3s后关闭
+# 关闭
+chrome.quit()
