d6205652251c09026a2e0ca629ce60adb0cb3553
like <980650920@qq.com>
2021-02-18 11:41:21 +0800

    猫眼爬虫 v2.0（一次开启多个）

--- src/MAOYAN/MAOYAN/spiders/maoyan.py
+++ src/MAOYAN/MAOYAN/spiders/maoyan.py
@@ -7,24 +7,27 @@ class MaoyanSpider(scrapy.Spider):
     name = 'maoyan'
     allowed_domains = ['www.maoyan.com']
     offset = 0
-    start_urls = ['https://maoyan.com/board/4']
+    start_urls = ['https://maoyan.com/board/4?offset=0']
 
     def parse(self, response):
+        for offset in range(0, 91, 10):
+            url = 'https://maoyan.com/board/4?offset={}'.format(offset)
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse_html,
+                dont_filter=True  # 很关键
+            )
+        print("start")
+
+    def parse_html(self, response):
+        print("123")
         ddList = response.xpath('//dl[@class="board-wrapper"]/dd')
         item = MaoyanItem()
         for dd in ddList:
             item['name'] = dd.xpath('./a/@title').extract_first().strip()
             item['star'] = dd.xpath('.//p[@class="star"]/text()').extract_first().strip()
             item['time'] = dd.xpath('.//p[@class="releasetime"]/text()').extract_first().strip()
+
+            print(item['name'], item['star'], item['time'])
             # 把爬取的数据交给pipeLine
             yield item
-
-        # 拼接url 交给调度器入队列
-        self.offset += 10
-        if self.offset <= 90:
-            url = 'https://maoyan.com/board/4?offset={}'.format(self.offset)
-            print(url)
-            yield scrapy.Request(
-                url=url,
-                callback=self.parse
-            )
