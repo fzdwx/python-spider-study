3b687eef21fc61442117d9d9cc1eaeca02f163e4
like <980650920@qq.com>
2021-02-18 12:42:52 +0800

    猫眼爬虫 v3.0（通过重写start_requests方法来加入url）

--- src/MAOYAN/MAOYAN/spiders/maoyan.py
+++ src/MAOYAN/MAOYAN/spiders/maoyan.py
@@ -5,22 +5,24 @@ from src.MAOYAN.MAOYAN.items import MaoyanItem
 
 class MaoyanSpider(scrapy.Spider):
     name = 'maoyan'
-    allowed_domains = ['www.maoyan.com']
+    allowed_domains = ['maoyan.com']
     offset = 0
     start_urls = ['https://maoyan.com/board/4?offset=0']
 
     def parse(self, response):
+        pass
+
+    # 把所有url地址都交给调度器
+    def start_requests(self):
         for offset in range(0, 91, 10):
             url = 'https://maoyan.com/board/4?offset={}'.format(offset)
             yield scrapy.Request(
                 url=url,
                 callback=self.parse_html,
-                dont_filter=True  # 很关键
+                # dont_filter=True  # 很关键
             )
-        print("start")
 
-    def parse_html(self, response):
-        print("123")
+    def parse_html(self,response):
         ddList = response.xpath('//dl[@class="board-wrapper"]/dd')
         item = MaoyanItem()
         for dd in ddList:
@@ -28,6 +30,5 @@ class MaoyanSpider(scrapy.Spider):
             item['star'] = dd.xpath('.//p[@class="star"]/text()').extract_first().strip()
             item['time'] = dd.xpath('.//p[@class="releasetime"]/text()').extract_first().strip()
 
-            print(item['name'], item['star'], item['time'])
             # 把爬取的数据交给pipeLine
             yield item
