9974ff9a7674ccbf1421e4561fcf7baa5b57d068
like <980650920@qq.com>
2021-02-19 11:06:38 +0800

    增加保存到数据库中

--- src/MAOYAN/MAOYAN/pipelines.py
+++ src/MAOYAN/MAOYAN/pipelines.py
@@ -6,9 +6,36 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymysql
+from .settings import *
 
 
 class MaoyanPipeline:
     def process_item(self, item, spider):
-        print(item['name'],item['star'],item['time'])
+        # print(item['name'], item['star'], item['time'])
         return item
+
+
+class MysqlPipeline(object):
+    def __init__(self):
+        pass
+
+    def open_spider(self, spider):
+        self.db = pymysql.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            db=MYSQL_DB)
+        self.cursor = self.db.cursor()
+        pass
+
+    def process_item(self, item, spider):
+        sql = 'insert into maoyan(`name`,`star`,`time`) values(%s,%s,%s)'
+        data = [item['name'], item['star'], item['time']]
+        self.cursor.execute(sql, data)
+        self.db.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.db.close()
