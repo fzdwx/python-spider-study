577d30a874e36ea24dbe0a2efa6533d0473044ae
like <980650920@qq.com>
2021-02-20 12:33:48 +0800

    使用中间件生成user-agent项目初始化

---
+++ src/Baidu/Baidu/spiders/baidu.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['www.baidu.com']
+    start_urls = ['http://www.baidu.com/']
+
+    def parse(self, response):
+        print('parse函数被调用')
+        pass
