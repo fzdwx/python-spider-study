bfbc7734a960c7dec15cca9d0ecd9d4202d95d1b
like <980650920@qq.com>
2021-02-20 12:46:13 +0800

    使用中间件生成user-agent

--- src/Baidu/Baidu/middlewares.py
+++ src/Baidu/Baidu/middlewares.py
@@ -7,6 +7,7 @@ from scrapy import signals
 
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
+from fake_useragent import UserAgent
 
 
 class BaiduSpiderMiddleware:
@@ -101,4 +102,12 @@ class BaiduDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class RandomUserAgentDownloaderMiddleware(object):
+    def process_request(self, request, spider):
+        ua = UserAgent()
+        userAgent = ua.random
+        request.headers['User-Agent'] = userAgent
+        print(userAgent)
 
--- src/Baidu/Baidu/settings.py
+++ src/Baidu/Baidu/settings.py
@@ -17,10 +17,10 @@ NEWSPIDER_MODULE = 'Baidu.spiders'
 #USER_AGENT = 'Baidu (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -50,9 +50,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'Baidu.middlewares.BaiduDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'Baidu.middlewares.BaiduDownloaderMiddleware': 543,
+   'Baidu.middlewares.RandomUserAgentDownloaderMiddleware': 143,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
