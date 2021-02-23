6852433c0d0ea4a07fbb8e5ae258af817c744246
like <980650920@qq.com>
2021-02-23 11:36:41 +0800

    添加x,y,title的描述信息

--- 数据分析/matplotlib/03.demo.py
+++ 数据分析/matplotlib/03.demo.py
@@ -17,12 +17,19 @@ plt.figure(figsize=(20, 8), dpi=80)
 
 plt.plot(x, y)
 
-chFont = font_manager.FontProperties(fname="C:/Windows/Fonts/SIMHEI.TTF")
+# 中文字体
+chFont = font_manager.FontProperties(family="SimHei")  # SimHei
+# chFont = font_manager.FontProperties(fname="C:/Windows/Fonts/SIMHEI.TTF")
 
-# 跳转x轴的刻度
+# 刻度相关设置
 step = 10
 xLabels = ["10点,{}分".format(i) for i in range(60)]
 xLabels += ["11点,{}分".format(i) for i in range(60)]
 plt.xticks(list(x)[::step], xLabels[::step], rotation=25, fontProperties=chFont)
+
+# 添加描述信息
+plt.xlabel("时间", fontProperties=chFont)
+plt.ylabel("温度 单位(℃)", fontProperties=chFont)
+plt.title("10点到12点每分钟的气温变化", fontProperties=chFont)
 
 plt.show()
